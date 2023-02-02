ef9dd2aeda36578e4a3a504d4de26f443c91f6f4
Salman Saleem <salmansaleem0302@gmail.com>
2023-02-03 02:44:50 +0500

    Added OnlineMapsV3 asset (Infinity Code)

---
+++ Trace/Assets/_ThirdParty/Infinity Code/Online maps/Examples (API usage)/HereRoutingAPIExample.cs
@@ -0,0 +1,69 @@
+﻿/*         INFINITY CODE         */
+/*   https://infinity-code.com   */
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace InfinityCode.OnlineMapsExamples
+{
+    /// <summary>
+    /// Example of a request to HERE Routing API.
+    /// </summary>
+    [AddComponentMenu("Infinity Code/Online Maps/Examples (API Usage)/HereRoutingAPIExample")]
+    public class HereRoutingAPIExample : MonoBehaviour
+    {
+        private void Start()
+        {
+            // Looking for public transport route between the coordinates.
+            OnlineMapsHereRoutingAPI.Find(
+                new OnlineMapsHereRoutingAPI.Waypoint(37.38589, 55.90042), // Origin
+                new OnlineMapsHereRoutingAPI.Waypoint(37.6853002, 55.8635228), // Destination
+                new Dictionary<string, string>
+                {
+                    {"transportMode", "bus" },
+                    {"lang",  "ru-ru"},
+                    {"alternatives", "3" },
+                    {"return", "polyline,actions,instructions" }
+                }
+                ).OnComplete += OnComplete;
+        }
+
+        /// <summary>
+        /// This method is called when a response is received.
+        /// </summary>
+        /// <param name="response">Response string</param>
+        private void OnComplete(string response)
+        {
+            Debug.Log(response);
+
+            // Get result object
+            OnlineMapsHereRoutingAPIResult result = OnlineMapsHereRoutingAPI.GetResult(response);
+
+            if (result != null)
+            {
+                Color[] colors =
+                {
+                    Color.green,
+                    Color.red,
+                    Color.blue,
+                    Color.magenta
+                };
+                int colorIndex = 0;
+
+                // Draw all the routes in different colors.
+                foreach (OnlineMapsHereRoutingAPIResult.Route route in result.routes)
+                {
+                    foreach (OnlineMapsHereRoutingAPIResult.Section section in route.sections)
+                    {
+                        if (section.polylinePoints2d != null)
+                        {
+                            OnlineMapsDrawingElement line = new OnlineMapsDrawingLine(section.polylinePoints2d, colors[colorIndex++]);
+                            OnlineMapsDrawingElementManager.AddItem(line);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Trace/Assets/_ThirdParty/Infinity Code/Online maps/Scripts/Editor/Elevations/OnlineMapsArcGISElevationManagerEditor.cs
@@ -0,0 +1,29 @@
+﻿/*         INFINITY CODE         */
+/*   https://infinity-code.com   */
+
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(OnlineMapsArcGISElevationManager), true)]
+public class OnlineMapsArcGISElevationManagerEditor : OnlineMapsSinglePartElevationManagerEditor
+{
+    private SerializedProperty resolution;
+
+    protected override void CacheSerializedFields()
+    {
+        base.CacheSerializedFields();
+
+        resolution = serializedObject.FindProperty("resolution");
+    }
+
+    protected override void GenerateLayoutItems()
+    {
+        base.GenerateLayoutItems();
+
+        LayoutItem item = new LayoutItem("WARNING");
+        item.action += () => EditorGUILayout.HelpBox("ArcGIS has closed its elevations services. This component will not work and is present for compatibility. Replace it to some other Elevation Manager.", MessageType.Error);
+        rootLayoutItem.Insert(0, item);
+
+        rootLayoutItem.Create(resolution).OnChanged += () => { resolution.intValue = Mathf.Clamp(resolution.intValue, 16, 100); };
+    }
+}
