a95fdb9cba403f43b1bd06e6a3141998e7ff8c1b
Salman Saleem <salmansaleem0302@gmail.com>
2023-02-20 16:44:36 +0500

    - ViewController inplemented in RegisterCanvas. - Validations completed

---
+++ Trace/Assets/Scripts/CanvasManagers/RegisterCanvas/RegisterCanvas.cs
@@ -0,0 +1,63 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CanvasManagers
+{
+    public class RegisterCanvas : MonoBehaviour
+    {
+        [Header("Canvas Components")]
+        public TMP_InputField usernameText;
+        public TMP_InputField emailText;
+        public TMP_InputField passwordText;
+        public TMP_InputField retypePasswordText;
+        public Button registerButton;
+        public TMP_Text errorText;
+
+
+        private RegisterCanvasController _controller;
+        
+        #region UnityCallbacks
+
+        private void OnEnable()
+        {
+            Debug.Log("Register Canvas Enabled");
+
+            if (_controller == null)
+                _controller = new RegisterCanvasController(this);
+            
+            _controller.Init();
+            registerButton.interactable = false;
+        }
+
+        private void OnDisable()
+        {
+            _controller.Uninitialise();
+        }
+
+        #endregion
+
+
+        private void Register(string username, string email, string password, string confirmPassword)
+        {
+            StartCoroutine(FbManager.instance.RegisterNewUser(email, password, username, "", (response) =>
+            {
+                Debug.Log("Registered Response received from Firebase: " + response);
+            }));
+        }
+
+        public void ShowMessage(string message)
+        {
+            errorText.text = message;
+            errorText.gameObject.SetActive(true);
+            
+            StartCoroutine(HelperMethods.TimedActionFunction(3f, ()=>
+            {
+                errorText.text = "";
+                errorText.gameObject.SetActive(false);
+            }));
+        }
+        
+    }
+}
