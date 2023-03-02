2c1e887e35e5560023e252c68a9706324a6059c6
Salman Saleem <salmansaleem0302@gmail.com>
2023-03-03 01:18:05 +0500

    Phone Code Verification

--- Trace/Assets/Scripts/CanvasManagers/PhoneNumberVerificationCanvas/Enter Code Canvas/EnterCodeCanvas.cs
+++ Trace/Assets/Scripts/CanvasManagers/PhoneNumberVerificationCanvas/Enter Code Canvas/EnterCodeCanvas.cs
@@ -13,41 +13,22 @@ namespace CanvasManagers
         public TMP_InputField _verificationCode;
         public Button _submitButton;
         public Button _requestNewCodeButton;
+        public Button _backButton;
 
 
         private EnterCodeCanvasController _controller;
+        private string phoneNumber = "";
 
-
-
-        public void OnEditVerificationCode()
+        public void Init(string numberStr)
         {
-            var vCode = _verificationCode.text;
-
-            if (vCode.Length < 6)
-            {
-                _submitButton.interactable = false;
-            }
-            
-            if (vCode.Length == 6)
-            {
-                _verificationCode.DeactivateInputField();
-                _submitButton.interactable = true;
-            }
-
-            if (vCode.Length > 6)
-            {
-                _verificationCode.text = _verificationCode.text.Substring(0, _verificationCode.text.Length - 1);
-            }
-            
+            phoneNumber = numberStr;
         }
-        
-        
 
         private void OnEnable()
         {
             if (_controller is null)
                 _controller = new EnterCodeCanvasController(this);
-            
+
             _submitButton.interactable = false;
             _controller.Init();
         }
--- Trace/Assets/Scripts/CanvasManagers/PhoneNumberVerificationCanvas/PhoneNumberTextCanavs.cs
+++ Trace/Assets/Scripts/CanvasManagers/PhoneNumberVerificationCanvas/PhoneNumberTextCanavs.cs
@@ -10,58 +10,13 @@ namespace CanvasManagers
     public class PhoneNumberTextCanavs : MonoBehaviour
     {
         public TMP_InputField _numberInputField;
-        [SerializeField] private TMP_Text _phoneNumberDisplay;
+        [SerializeField] public TMP_Text _phoneNumberDisplay;
         [SerializeField] private int lastLength;
         public Button verifyNumberButton;
-
+        public TMP_InputField _countryCodeInputField;
+        public EnterCodeCanvas _numberValidationView;
+        public GameObject validationScreen;
         private PhoneNumberCanvasController _controller;
-
-        public void editPhoneNumber()
-        {
-            var phoneNumber = _numberInputField.text;
-            _phoneNumberDisplay.text = phoneNumber;
-
-
-            //first dash in phone number
-            if (phoneNumber.Length > 3)
-            {
-                var phoneNumberVisual = _phoneNumberDisplay.text.Insert(3, "-");
-                _phoneNumberDisplay.text = phoneNumberVisual;
-            }
-
-            if (phoneNumber.Length > 7)
-            {
-                var phoneNumberVisual = _phoneNumberDisplay.text.Insert(7, "-");
-                _phoneNumberDisplay.text = phoneNumberVisual;
-            }
-
-            
-            //second dash in phone number
-            // if (phoneNumber.Length == 7 && lastLength != 8)
-            // {
-            //     var phoneNumberVisual = _inputField.text + "-";
-            //     _inputField.text = phoneNumberVisual;
-            //     _inputField.MoveToEndOfLine(false, false);
-            // }else if (phoneNumber.Length == 7 && lastLength == 8) //if delete key pressed
-            // {
-            //     _phoneNumberDisplay.text = _inputField.text.Substring(0, _inputField.text.Length - 1);
-            // }
-
-
-            //once at length deselect input field
-            if (phoneNumber.Length == 10)
-            {
-                _numberInputField.DeactivateInputField();
-            }
-
-            //keeps phone number length correct
-            if (phoneNumber.Length > 10)
-            {
-                _numberInputField.text = _numberInputField.text.Substring(0, _numberInputField.text.Length - 1);
-            }
-
-            lastLength = _numberInputField.text.Length;
-        }
 
         
         #region UnityCallbacks
