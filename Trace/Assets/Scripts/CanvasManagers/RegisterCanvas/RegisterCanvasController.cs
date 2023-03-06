8a5485b1219eb4392ec6326be5e93ffb8ccdc394
Salman Saleem <salmansaleem0302@gmail.com>
2023-03-06 18:29:38 +0500

    Code Verification Done

--- Trace/Assets/Scripts/CanvasManagers/RegisterCanvas/RegisterCanvasController.cs
+++ Trace/Assets/Scripts/CanvasManagers/RegisterCanvas/RegisterCanvasController.cs
@@ -44,9 +44,16 @@ namespace CanvasManagers
         private void OnClickRegister()
         {
             _view.LoadingState(true);
-            var email = _view.emailText.text.Replace('.', ',');
+            string email = "";
+
+#if UNITY_EDITOR
+             email = _view.emailText.text;
+#else
+             email = _view.emailText.text.Replace('.', ',');
+#endif
+            
             Debug.LogError("Email is ::  "+ email);
-            _view.StartCoroutine(FbManager.instance.RegisterNewUser(email, _view.passwordText.text, _view.usernameText.text, "", (response, errorCode) =>
+            _view.StartCoroutine(FbManager.instance.RegisterNewUser(email, _view.passwordText.text, _view.usernameText.text, "+923407631749", (response, errorCode) =>
             {
                  _view.LoadingState(false);
                 Debug.Log("Registered Response received from Firebase: " + response);
