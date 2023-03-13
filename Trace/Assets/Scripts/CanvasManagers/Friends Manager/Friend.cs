070fa6940439549ed88fae0925770b1e267392fb
Salman Saleem <salmansaleem0302@gmail.com>
2023-03-13 16:02:13 +0500

    Friends Search Commit

---
+++ Trace/Assets/Scripts/CanvasManagers/Friends Manager/Friend.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Friend : MonoBehaviour
+{
+    [SerializeField] private Image _profilePic;
+    [SerializeField] private TMP_Text _nickName;
+    [SerializeField] private TMP_Text _userName;
+    [SerializeField] public Button _addRemoveButton;
+    [SerializeField] private Image _buttonBackground;
+    [SerializeField] private Color[] _colors;
+    
+
+
+    public void UpdateFrindData()
+    {
+        
+    }
+}
