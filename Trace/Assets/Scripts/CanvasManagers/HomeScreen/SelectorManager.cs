0524e262ef21143241f53613a4a10069c7c15350
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2023-03-23 17:38:55 -0700

    added functionality to HomeScreen

---
+++ Trace/Assets/Scripts/CanvasManagers/HomeScreen/SelectorManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorManager : MonoBehaviour
+{
+    [SerializeField] private Animator selectorAnimator;
+    [SerializeField] private bool selectorInUpPosition;
+    public void OnEnable()
+    {
+        
+    }
+
+    public void SelectorPressed()
+    {
+        selectorInUpPosition = selectorInUpPosition ? false : true;
+        if (!selectorInUpPosition)
+        {
+            selectorAnimator.Play("MoveDown");
+        }
+        else
+        {
+            selectorAnimator.Play("MoveUp");
+        }
+    }
+
+}
