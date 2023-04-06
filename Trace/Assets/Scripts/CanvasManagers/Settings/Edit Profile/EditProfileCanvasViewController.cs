b6fdd35cd31f5a3fbb83155ea8ec231bb1e65cc6
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2023-04-06 18:05:35 -0400

    fixed merge bug

--- Trace/Assets/Scripts/CanvasManagers/Settings/Edit Profile/EditProfileCanvasViewController.cs
+++ Trace/Assets/Scripts/CanvasManagers/Settings/Edit Profile/EditProfileCanvasViewController.cs
@@ -21,13 +21,13 @@ public class EditProfileCanvasViewController
 
     private void UpdateData()
     {
-        _view._email.text = FbManager.instance._currentUser.Email;
-        _view._username.text = FbManager.instance._currentUser.Username;
-        _view._usernameLarge.text = FbManager.instance._currentUser.Username;
-        _view._profileName.text = FbManager.instance._currentUser.DisplayName;
-        _view._profileNameLarge.text = FbManager.instance._currentUser.DisplayName;
-        _view._password.text = FbManager.instance._currentUser.Password;
-        FbManager.instance._currentUser.ProfilePicture(sprite =>
+        _view._email.text = FbManager.instance.thisUserModel.Email;
+        _view._username.text = FbManager.instance.thisUserModel.Username;
+        _view._usernameLarge.text = FbManager.instance.thisUserModel.Username;
+        _view._profileName.text = FbManager.instance.thisUserModel.DisplayName;
+        _view._profileNameLarge.text = FbManager.instance.thisUserModel.DisplayName;
+        _view._password.text = FbManager.instance.thisUserModel.Password;
+        FbManager.instance.thisUserModel.ProfilePicture(sprite =>
         {
             _view._profilePicture.sprite = sprite;
         });
